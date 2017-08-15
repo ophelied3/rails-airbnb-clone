afd50ff851f4c221af04c2a9fc47ed5ab3d95425
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-15 14:48:05 +0200

    Modification du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Bookings.destroy_all
+Booking.destroy_all
 Horse.destroy_all
 User.destroy_all
 
