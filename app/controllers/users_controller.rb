021020b40ddd52db4ce8d2d39c2312ef58e7159b
Jerome Vivier <jerome.vivier@edhec.com>
2017-08-15 17:32:22 +0200

    profil du user fini sans Ajax (grace à Arnaud et Jerome)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @bookings = current_user.bookings
+    @horses = current_user.horses
+    params[:information]
   end
 
 end
