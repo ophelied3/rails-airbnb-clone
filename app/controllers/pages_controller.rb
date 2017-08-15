d48d534a02bc60574cb1f16ef5c30c8008f23e3c
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-15 15:14:47 +0200

    Rajout de l'authentification pour pouvoir accèder aux pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
-    if user_signed_in?
-      redirect_to horses_path
-    end
   end
 end
