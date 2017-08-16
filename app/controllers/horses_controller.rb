6a5dd53e35dc4494b3cc1bbb10ea0f55d2c74e7b
jules maregiano <jules.maregiano@gmail.com>
2017-08-16 12:03:15 +0200

    a supprimer

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -2,13 +2,8 @@ class HorsesController < ApplicationController
   before_action :find_horse, only: [:show, :edit, :update, :destroy]
   before_action :all_horse, only: [:index]
   skip_before_action :authenticate_user!, only: [:index, :show]
-  
+
   def index
-    if params[:address]
-      @horse_search = search(params[:address])
-      else
-      @horse_search = Horse.all.order('created_at DESC')
-    end
   end
 
   def show
