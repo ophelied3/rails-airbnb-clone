4a42468146655712aba92acff80a453d79668094
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-17 11:16:34 +0200

    Oublie

--- db/migrate/20170817090729_add_average_rating_to_horses.rb
+++ db/migrate/20170817090729_add_average_rating_to_horses.rb
@@ -1,5 +1,5 @@
 class AddAverageRatingToHorses < ActiveRecord::Migration[5.0]
   def change
-    add_column :horses, :average_rating, :float
+    add_column :horses, :average_rating, :float, :default => 5
   end
 end
