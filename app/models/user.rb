aed03fb57f3652662fc3959a2dd773ee663018ab
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-18 17:10:39 +0200

    Toujours plus chiant avec vos codes again

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,12 @@ class User < ApplicationRecord
     user_params = user_params.to_h
 
     user = User.find_by(provider: auth.provider, uid: auth.uid)
-    user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
+    user ||= User.find_by(email: auth.info.email)
     if user
       user.update(user_params)
     else
       user = User.new(user_params)
-      user.password = Devise.friendly_token[0, 20] # Fake password for validation
+      user.password = Devise.friendly_token[0, 20]
       user.save
     end
     user
