d46414badd9e156da231afa45b1a0559290cca13
Ophelie Delrieu <ophelie.delrieu3@orange.fr>
2017-08-18 15:17:41 +0200

    un peu de css sur la home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @horses = Horse.all.sample(3)
+    @horses = Horse.all.sample(6)
   end
 end
