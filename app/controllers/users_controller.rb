d1df9d4cc54c344bd8491dd291beee0efda67937
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-18 15:08:05 +0200

    Ajout d'un acces au reservation en attente depuis l'interface utilisateur (proprietaire)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,22 @@ class UsersController < ApplicationController
     if current_user == @user
       @bookings = current_user.bookings
       @horses = current_user.horses
+      @horses_booked = booked_horse
       params[:information]
     else
       redirect_to horses_path
     end
   end
+end
+
+private
+
+def booked_horse
+  horses_booked = []
+  @horses.each do |horse|
+    horse.bookings.each do |booking|
+      horses_booked << booking
+    end
+  end
+  horses_booked
 end
